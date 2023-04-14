6b758cc3d847e7793c7533f132aed6a5646764c6
Rampat3 <dennyfells13@gmail.com>
2023-04-14 19:11:08 +0700

    added navMenu

--- src/contents/header.php
+++ src/contents/header.php
@@ -1,8 +1,77 @@
-<div class="navMenu">
-    <div>
+<section class="header">
+    <div class="navMenu">
+        <div class="navMenu__icon">
+            <img src="../src/icons/header-icon.png">
+        </div>
+        <div class="navMenu__menu">
+            <ul>
+                <li>
+                    <select>
+                        <option value="">Home</option>
+                        <option value="">Home</option>
+                        <option value="">Home</option>
+                    </select>
+                </li>
+                <li>
+                    <select>
+                        <option value="">Features</option>
+                        <option value="">Features</option>
+                        <option value="">Features</option>
+                    </select>
+                </li>
+                <li>
+                    <select>
+                        <option value="">Reviews</option>
+                        <option value="">Reviews</option>
+                        <option value="">Reviews</option>
+                    </select>
+                </li>
+                <li>
+                    <select>
+                        <option value="">Pricing</option>
+                        <option value="">Pricing</option>
+                        <option value="">Pricing</option>
+                    </select>
+                </li>
+                <li>
+                    <a href="">Jobs</a>
+                </li>
+            </ul>
+        </div>
+        <div class="navMenu__register">
+            <ul>
+                <li><img src="../src/icons/user-icon.png" alt=""></li>
+                <li>
+                    <!-- Button trigger modal -->
+                    <button type="button" class="navMenu__register-button" data-toggle="modal" data-target="#exampleModal">
+                        Sign In
+                    </button>
 
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    ...
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="navMenu__free-trial">
+            <button type="button">Start Free Trial →</button>
+        </div>
     </div>
-    <div>
-
-    </div>
-</div>
+</section>
