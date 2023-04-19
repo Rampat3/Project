cb6cfc004521499fcbe017f67241c51fadda2735
Rampat3 <dennyfells13@gmail.com>
2023-04-19 21:30:39 +0700

    Trial

---
+++ src/contents/main/business.php
@@ -0,0 +1,209 @@
+<div class="business__wrapper">
+    <div class="business__wrapper-title">
+        <div class="business__wrapper-title-header title__text">
+            Trusted By Thoudsands <p class="title__text-bg" style="margin: 0 0 0 10px;">Business</p>
+        </div>
+        <div class="business__wrapper-title-text">
+            <p class="title__text-p">
+                More Than 15,000 companies trust and choose Itech
+            </p>
+        </div>
+    </div>
+</div>
+
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.js"></script>
+
+
+
+<div class="wrapper">
+    <div class="item">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">Slide 1</div>
+                <div class="swiper-slide">Slide 2</div>
+                <div class="swiper-slide">Slide 3</div>
+            </div>
+
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    <div class="item">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">Slide 1</div>
+                <div class="swiper-slide">Slide 2</div>
+                <div class="swiper-slide">Slide 3</div>
+            </div>
+
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    <div class="item">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">Slide 1</div>
+                <div class="swiper-slide">Slide 2</div>
+                <div class="swiper-slide">Slide 3</div>
+            </div>
+
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    <div class="item">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">Slide 1</div>
+                <div class="swiper-slide">Slide 2</div>
+                <div class="swiper-slide">Slide 3</div>
+            </div>
+
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    <div class="item">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">Slide 1</div>
+                <div class="swiper-slide">Slide 2</div>
+                <div class="swiper-slide">Slide 3</div>
+            </div>
+
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    <div class="item">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">Slide 1</div>
+                <div class="swiper-slide">Slide 2</div>
+                <div class="swiper-slide">Slide 3</div>
+            </div>
+
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    <div class="item">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">Slide 1</div>
+                <div class="swiper-slide">Slide 2</div>
+                <div class="swiper-slide">Slide 3</div>
+            </div>
+
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    <div class="item">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">Slide 1</div>
+                <div class="swiper-slide">Slide 2</div>
+                <div class="swiper-slide">Slide 3</div>
+            </div>
+
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    <div class="item">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">Slide 1</div>
+                <div class="swiper-slide">Slide 2</div>
+                <div class="swiper-slide">Slide 3</div>
+            </div>
+
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+
+</div>
+<script>
+    // if ($('.swiper-container').length > 0) { //some-slider-wrap-in
+    //     let swiperInstances = [];
+    //     $(".swiper-container").each(function(index, element){ //some-slider-wrap-in
+    //         const $this = $(this);
+    //         $this.addClass("instance-" + index); //instance need to be unique (ex: some-slider)
+    //         $this.parent().find(".swiper-pagination").addClass("pagination-" + index);
+    //         $this.parent().find(".swiper-button-prev").addClass("prev-" + index); //prev must be unique (ex: some-slider-prev)
+    //         $this.parent().find(".swiper-button-next").addClass("next-" + index); //next must be unique (ex: some-slider-next)
+    //         swiperInstances[index] = new Swiper(".instance-" + index, { //instance need to be unique (ex: some-slider)
+    //             // your settings ...
+    //             navigation: {
+    //                 prevEl: ".prev-" + index,  //prev must be unique (ex: some-slider-prev)
+    //                 nextEl: ".next-" + index, //next must be unique (ex: some-slider-next)
+    //             },
+    //             pagination: {
+    //                 el: '.pagination-' + index,
+    //                 type: 'bullets',
+    //                 clickable: true
+    //             },
+    //         });
+    //     });
+    //
+    //     // Now you can call the update on a specific instance in the "swiperInstances" object
+    //     // e.g.
+    //     swiperInstances[3].update();
+    //     //or all of them
+    //     setTimeout(function () {
+    //         for (const slider of swiperInstances) {
+    //             slider.update();
+    //         }
+    //     }, 50);
+    // }
+    var mySwiper = new Swiper ('.swiper-container', {
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+    })
+
+</script>
+
+<style>
+    * {
+        box-sizing: border-box;
+    }
+    html, body {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+    }
+    .wrapper {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        align-items: center;
+        height: 100%;
+        width: 100%;
+        background: #d3d3d3;
+    }
+    .item {
+        height: 250px;
+        width: 30%;
+        background: #fff;
+
+    }
+    .swiper-container {
+        width: 100%;
+        height: 100%;
+    }
+    .swiper-slide {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
