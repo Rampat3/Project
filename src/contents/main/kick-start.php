3e562374d5509e6fa10f02b8379920a828be7b40
Rampat3 <dennyfells13@gmail.com>
2023-05-17 01:05:53 +0700

    Added block kick-start

---
+++ src/contents/main/kick-start.php
@@ -0,0 +1,21 @@
+<div class="kick-start__wrapper">
+    <div class="kick-start__wrapper-title">
+        <div class="kick-start__wrapper-title-header title__text">
+            Choose Plan and <p class="title__text-bg" style="margin: 0 0 0 10px">Kick-start</p>
+        </div>
+        <div class="kick-start__wrapper-title-text">
+            <p class="title__text-p" style="margin: 50px 0">Whether you're just getting started with rapid testing or scaling across the organization, we've got you covered.</p>
+        </div>
+    </div>
+    <div class="kick-start__wrapper-table table">
+        <div class="table__title">
+            <ul class="table__title-price">
+                <li>Billed Monthly</li>
+                <li style="background-color: white; border-radius: 50px; display: flex">Billed Yearly <p style="background-color: red">-15%</p></li>
+            </ul>
+        </div>
+        <div class="table__wrapper">
+
+        </div>
+    </div>
+</div>
