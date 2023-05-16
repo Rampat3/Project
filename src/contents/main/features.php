a7b79ececc4345fdf66cedcad34692fb9f1059c9
Rampat3 <dennyfells13@gmail.com>
2023-05-16 23:37:09 +0700

    update blocks features

--- src/contents/main/features.php
+++ src/contents/main/features.php
@@ -10,4 +10,70 @@
             </p>
         </div>
     </div>
+    <div class="features__wrapper-catalogs catalogs">
+        <div class="catalogs__wrapper">
+            <div class="catalogs__wrapper-items">
+                <img src="../src/img/features-items/items-1.jpg" alt="">
+                <a href="#" class="catalogs__wrapper-items-title">Store Hours and Vacation</a>
+                <p class="title__text-p" style="font-size: 15px">Ideal for businesses that are seasonal or operate between fixed hours.</p>
+            </div>
+        </div>
+        <div class="catalogs__wrapper">
+            <div class="catalogs__wrapper-items">
+                <img src="../src/img/features-items/items-2.jpg" alt="">
+                <a href="#" class="catalogs__wrapper-items-title">Coupon Management</a>
+                <p class="title__text-p" style="font-size: 15px">Set up irresistible deals and offers with flexible discount promo codes.</p>
+            </div>
+        </div>
+        <div class="catalogs__wrapper">
+            <div class="catalogs__wrapper-items">
+                <img src="../src/img/features-items/items-3.jpg" alt="">
+                <a href="#" class="catalogs__wrapper-items-title">Product Import and Export</a>
+                <p class="title__text-p" style="font-size: 15px">Import products from and
+                    export products to CSV from
+                    vendor dashboard.</p>
+            </div>
+        </div>
+        <div class="catalogs__wrapper">
+            <div class="catalogs__wrapper-items">
+                <img src="../src/img/features-items/items-4.jpg" alt="">
+                <a href="#" class="catalogs__wrapper-items-title">Inquiry and Support Ticker System</a>
+                <p class="title__text-p" style="font-size: 15px">Interaction with sellers help buyers to make better purchase decisions.</p>
+            </div>
+        </div>
+        <div class="catalogs__wrapper">
+            <div class="catalogs__wrapper-items">
+                <img src="../src/img/features-items/items-5.jpg" alt="">
+                <a href="#" class="catalogs__wrapper-items-title">Membership Subscription</a>
+                <p class="title__text-p" style="font-size: 15px">Offer membership plans for
+                    sellers, free and paid. Accept recurring fees.</p>
+            </div>
+        </div>
+        <div class="catalogs__wrapper">
+            <div class="catalogs__wrapper-items">
+                <img src="../src/img/features-items/items-6.jpg" alt="">
+                <a href="#" class="catalogs__wrapper-items-title">Invoice and Shipping Labels</a>
+                <p class="title__text-p" style="font-size: 15px">Order fulfillment made easy
+                    with store invoices and shipping labels.</p>
+            </div>
+        </div>
+        <div class="catalogs__wrapper">
+            <div class="catalogs__wrapper-items">
+                <img src="../src/img/features-items/items-7.jpg" alt="">
+                <a href="#" class="catalogs__wrapper-items-title">GEO Location and Radius Search</a>
+                <p class="title__text-p" style="font-size: 15px">Allow searching products
+                    or services near the user's
+                    current location.</p>
+            </div>
+        </div>
+        <div class="catalogs__wrapper">
+            <div class="catalogs__wrapper-items">
+                <img src="../src/img/features-items/items-8.jpg" alt="">
+                <a href="#" class="catalogs__wrapper-items-title">Shipment and Delivery</a>
+                <p class="title__text-p" style="font-size: 15px">Share tracking of deliveries,
+                    notify when shipping
+                    status changes.</p>
+            </div>
+        </div>
+    </div>
 </div>
